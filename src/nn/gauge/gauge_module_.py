b641040e43de3749ea1a285190381b2ef84c93ba
jkomijani <jkomijani@gmail.com>
2024-01-16 14:44:21 +0100

    _hack method updated.

--- src/nn/gauge/gauge_module_.py
+++ src/nn/gauge/gauge_module_.py
@@ -209,6 +209,10 @@ class SVDGaugeModule_(StapledMatrixModule_):
 
     matrix_handle: class instance
         to handle matrices as expected in the supper class `MatrixModule_`.
+
+    IMPORTANT NOTE:
+        in order to have an invertible forward method, the masks used in
+        `param_net_` and `dual_param_net_` must be compatible.
     """
 
     unbounded_vector_axis = True
@@ -278,7 +282,7 @@ class SVDGaugeModule_(StapledMatrixModule_):
 
         return x, logJ
 
-    def _hack(self, x, log0=0, forward=True):
+    def _hack(self, x, forward=True):
         """Similar to the forward method, but returns intermediate parts."""
 
         if self.unbounded_vector_axis:
@@ -289,32 +293,29 @@ class SVDGaugeModule_(StapledMatrixModule_):
         staples = self.staples_handle.calc_staples(
                 x, mu=self.mu, nu_list=self.nu_list
                 )
-        stack = [(x_mu, staples)]
         slink, svd_ = self.staples_handle.staple(x_mu, staples=staples)
-        stack.append((slink, svd_))
 
         if forward:
-            slink_rotation, logJ = super().forward(
-                slink, log0=log0, svd_=svd_, reduce_=True
-                )
+            slink_rotation, logJ = super().forward(slink, svd_=svd_, reduce_=True)
         else:
-            slink_rotation, logJ = super().backward(
-                slink, log0=log0, svd_=svd_, reduce_=True
-                )
+            slink_rotation, logJ = super().backward(slink, svd_=svd_, reduce_=True)
 
-        stack.append(super()._hack(slink, forward=forward, reduce_=True))
-        stack.append((slink_rotation,))
+        stack = dict(
+                x_mu_initial = x_mu,
+                staples = staples,
+                slink = slink,
+                svd_ = svd_,
+                slink_rotation = slink_rotation,
+                logJ = logJ,
+                super_hack = super()._hack(
+                    slink, svd_=svd_, forward=forward, reduce_=True
+                    )
+                )
 
         x_mu = self.staples_handle.push2link(
                 x_mu, slink_rotation=slink_rotation, svd_=svd_
                 )
-        stack.append((x_mu,))
-
-        if self.unbounded_vector_axis:
-            x[self.mu] = x_mu
-        else:
-            x[:, self.mu] = x_mu
-        stack.append((x, logJ))
+        stack["x_mu_final"] = x_mu
 
         return stack
 
