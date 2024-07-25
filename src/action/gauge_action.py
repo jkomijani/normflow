f99923655eabbd74188621b8963f47c0732c69ef
jkomijani <jkomijani@gmail.com>
2024-07-25 11:42:29 +0200

    GaugeAction -> WilsonGaugeAction; nc > n_c; algebra_force is added to WilsonGaugeAction; GaugeAction is WilsonGaugeAction by default.

--- src/action/gauge_action.py
+++ src/action/gauge_action.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021-2022 Javad Komijani
+# Copyright (c) 2021-2024 Javad Komijani
 
 """This is a module for defining gauge actions..."""
 
@@ -7,18 +7,23 @@ import torch
 
 from numpy import pi
 
+from ..lib.matrix_handles import WilsonStaplesHandle
 
-class GaugeAction:
-    r"""The action is defined as
+
+# =============================================================================
+class WilsonGaugeAction:
+    r"""The Wilson gauge action is defined as
 
     .. math::
 
-        S = \int d^n x (...).
+        S = - \frac{\beta} {2 n_c} \sum_{\nu \neq \mu} Tr \text{Plaq}_{mu, nu}
+          = - \frac{\beta} {n_c}   \sum_{\nu < \mu} ReTr \text{Plaq}_{mu, nu} .
     """
-    def __init__(self, *, beta, ndim, nc):
+    def __init__(self, *, beta, ndim, n_c):
         self.beta = beta
         self.ndim = ndim
-        self.nc = nc  # number of colors, i.e. dimension of the gauge matrix
+        self.n_c = n_c  # number of colors of the gauge
+        self.staples_handle = WilsonStaplesHandle()
 
     def reset_parameters(self, *, beta):
         self.beta = beta
@@ -28,21 +33,39 @@ class GaugeAction:
   
     def action(self, cfgs):
         """Returns action corresponding to input configurations."""
-        dim = tuple(range(1, 1+self.ndim))  # 0 axis is the batch axis
-        action = 0
+        dim = tuple(range(1, 1 + self.ndim))  # 0 axis is the batch axis
+        sum_ = 0
         for mu in range(1, self.ndim):
             for nu in range(mu):
-                action -= torch.sum(self.calc_plaq(cfgs, mu=mu, nu=nu), dim=dim)
-        return self.beta * action
+                sum_ += torch.sum(self.calc_plaq(cfgs, mu=mu, nu=nu), dim=dim)
+        # 1 / n_c is considered in 'self.calc_plaq`; see `calc_reduced_trace`.
+        return - self.beta * sum_
 
     def action_density(self, cfgs):
         """Returns action density corresponding to input configurations."""
-        dim = tuple(range(1, 1+self.ndim))  # 0 axis is the batch axis
-        action_density = 0
+        dim = tuple(range(1, 1 + self.ndim))  # 0 axis is the batch axis
+        density = 0
         for mu in range(1, self.ndim):
             for nu in range(mu):
-                action_density -= self.calc_plaq(cfgs, mu=mu, nu=nu)
-        return self.beta * action_density
+                density += self.calc_plaq(cfgs, mu=mu, nu=nu)
+        # 1 / n_c is considered in 'self.calc_plaq`; see `calc_reduced_trace`.
+        return - self.beta * action_density
+
+    def force(self, links):
+        """Force is minus derivative of action w.r.t. gauge variables."""
+        return self.algebra_force(links) @ links
+
+    def algebra_force(self, links):
+        """Force is minus derivative of action w.r.t. (algebra) gauge variables
+        """
+        ndim = self.ndim
+        stps = torch.zeros_like(links)  # staples sum
+        for mu in range(ndim):
+            dict_ = dict(mu=mu, nu_list=[nu for nu in range(ndim) if nu != mu])
+            stps[:, mu] = self.staples_handle.calc_staples_sum(links, **dict_)
+        coeff = - self.beta / (2 * self.n_c)
+        algebra_force = coeff * anti_hermitian_traceless(links @ stps)
+        return algebra_force
 
     def calc_plaq(self, cfgs, *, mu, nu, real=True):
         x_mu = cfgs[:, mu]
@@ -70,7 +93,13 @@ class GaugeAction:
         return dict(beta=self.beta, ndim=self.ndim)
 
 
-class U1GaugeAction(GaugeAction):
+class GaugeAction(WilsonGaugeAction):
+    """For now the only supported gauge action is Wilson gauge action."""
+    pass
+
+
+# =============================================================================
+class U1WilsonGaugeAction(WilsonGaugeAction):
     """A special case of GaugeAction with special `plaq_rule`, ...."""
 
     def __init__(self, **kwargs):
@@ -92,9 +121,22 @@ class U1GaugeAction(GaugeAction):
         return topo_charge
 
 
+class U1GaugeAction(U1WilsonGaugeAction):
+    """For now the only supported gauge action is Wilson gauge action."""
+    pass
+
+
+# =============================================================================
 def calc_trace(x):
     return torch.sum(torch.diagonal(x, dim1=-2, dim2=-1), dim=-1)
 
 
 def calc_reduced_trace(x):  # reduced trace = 1/n trace()
     return torch.mean(torch.diagonal(x, dim1=-2, dim2=-1), dim=-1)
+
+
+def anti_hermitian_traceless(mtrx):
+    mtrx = (mtrx - mtrx.adjoint()) / 2.
+    mu = torch.mean(torch.linalg.diagonal(mtrx), dim=-1, keepdim=True)
+    mu = torch.diag_embed(torch.repeat_interleave(mu, mtrx.shape[-1], dim=-1))
+    return mtrx - mu
