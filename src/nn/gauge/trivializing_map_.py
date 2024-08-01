9a2b5a39dd45f927032f096289d2dc8040f01bc1
jkomijani <jkomijani@gmail.com>
2024-08-01 11:53:37 +0200

    WilsonTrivMap_ is added

---
+++ src/nn/gauge/trivializing_map_.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2024 Javad Komijani
+
+
+from torch_solve_ext.integrate import LieGroupAdjODEflow_
+from torch_solve_ext.integrate import LieGroupDynamicsAdjWrapper
+
+
+class WilsonTrivMap_(LieGroupAdjODEflow_):
+
+    def __init__(
+            self, wilson_action, t_span=[0, 1], step_size=None, order='LO'
+            ):
+
+        assert order == 'LO', "NLO is not implemented yet"
+
+        func = WilsonFlowDynamics(wilson_action)
+
+        if step_size is None:
+            step_size = 0.1 / wilson_action.beta  # 0.1 is good for beta = 1
+
+        super().__init__(func, t_span=t_span, step_size=step_size)
+
+
+class WilsonFlowDynamics(LieGroupDynamicsAdjWrapper):
+
+    def __init__(self, wilson_action):
+        super().__init__()
+        self.wilson_action = wilson_action
+        n_c = wilson_action.n_c
+        self.c_f = (n_c ** 2 - 1) / n_c  # (c_f = 8 / 3) for SU(3)
+
+    def algebra_dynamics(self, t, var, frozen_var):
+        force = self.wilson_action.algebra_force(var)
+        return force / (2 * self.c_f)
+
+    def calc_logj_rate(self, t, var, frozen_var):
+        # see e.g. (3.9) in [arXiv:0907.5491] or use `super().calc_logj_rate`.
+        return self.wilson_action(var)
