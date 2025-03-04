afcec0f60409a2dd79acc3c16bdd681d22982fa9
Javad Komijani <jkomijani@phys.ethz.ch>
2025-03-04 11:47:04 +0100

    version -> 2.0.0

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-# Copyright (c) 2021-2024 Javad Komijani
+# Copyright (c) 2021-2025 Javad Komijani
 
+"""This is the setup script for `normflow`."""
 
 from setuptools import setup
 
 
 def readme():
-    with open('README.rst') as f:
+    """Reads and returns the contents of the README.md file."""
+    with open('README.md', encoding='utf-8') as f:
         return f.read()
 
 
@@ -15,9 +17,9 @@ packages = [
         'normflow.device',
         'normflow.lib',
         'normflow.lib.combo',
-        'normflow.lib.matrix_handles',
         'normflow.lib.indexing',
         'normflow.lib.linalg',
+        'normflow.lib.matrix_handles',
         'normflow.lib.optim',
         'normflow.lib.spline',
         'normflow.lib.stats',
@@ -36,9 +38,9 @@ package_dir = {
         'normflow.device': 'src/device',
         'normflow.lib': 'src/lib',
         'normflow.lib.combo': 'src/lib/combo',
-        'normflow.lib.matrix_handles': 'src/lib/matrix_handles',
         'normflow.lib.indexing': 'src/lib/indexing',
         'normflow.lib.linalg': 'src/lib/linalg',
+        'normflow.lib.matrix_handles': 'src/lib/matrix_handles',
         'normflow.lib.optim': 'src/lib/optim',
         'normflow.lib.spline': 'src/lib/spline',
         'normflow.lib.stats': 'src/lib/stats',
@@ -51,15 +53,15 @@ package_dir = {
         'normflow.prior': 'src/prior'
         }
 
-setup(name='normflow',
-      version='1.1',
-      description='Normalizing flow for generating lattice field configurations',
-      packages=packages,
-      package_dir=package_dir,
-      url='http://github.com/jkomijani/normflow',
-      author='Javad Komijani',
-      author_email='jkomijani@gmail.com',
-      license='MIT',
-      # install_requires=['numpy>=2.0.0', 'torch>=2.3.1'],
-      zip_safe=False
-      )
+setup(
+    name='normflow',
+    version='2.0',
+    description='Normalizing flow for generating lattice field configurations',
+    packages=packages,
+    package_dir=package_dir,
+    url='http://github.com/jkomijani/normflow',
+    author='Javad Komijani',
+    author_email='jkomijani@gmail.com',
+    license='MIT',
+    zip_safe=False
+)
