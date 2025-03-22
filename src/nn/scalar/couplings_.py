6e7f26cf3e689141afeea432159f2ce96c55b0c9
Javad Komijani <jkomijani@gmail.com>
2025-03-22 19:31:54 +0100

    RQSplineCoupling_ is written using make_rq_spline_field()

--- src/nn/scalar/couplings_.py
+++ src/nn/scalar/couplings_.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021-2023 Javad Komijani
+# Copyright (c) 2021-2025 Javad Komijani
 
 """
 This module contains new neural networks that are subclasses of Module_ and
@@ -15,7 +15,7 @@ import numpy as np
 from abc import abstractmethod, ABC
 
 from .._core import Module_
-from ...lib.spline import RQSpline
+from ...lib.spline import make_rq_spline_field
 
 
 # =============================================================================
@@ -45,8 +45,8 @@ class Coupling_(Module_, ABC):
         Can be used for unique labeling of NNs.
     """
 
-    def __init__(self, nets, *, mask, channels_axis=1, label='coupling_'):
-        super().__init__(label=label)
+    def __init__(self, nets, *, mask, channels_axis=1):
+        super().__init__()
         self.nets = torch.nn.ModuleList(nets)
         self.mask = mask
         self.channels_axis = channels_axis
@@ -56,25 +56,25 @@ class Coupling_(Module_, ABC):
         for k, net in enumerate(self.nets):
             parity = k % 2
             x[parity], log0 = self.atomic_forward(
-                                                  x_active=x[parity],
-                                                  x_frozen=x[1 - parity],
-                                                  parity=parity,
-                                                  net=net,
-                                                  log0=log0
-                                                  )
+                x_active=x[parity],
+                x_frozen=x[1 - parity],
+                parity=parity,
+                net=net,
+                log0=log0
+            )
         return self.mask.cat(*x), log0
 
     def reverse(self, x, log0=0):
         x = list(self.mask.split(x))  # x = [x_0, x_1]
-        for k in list(range(len(self.nets)))[::-1]:
-            parity = k % 2
+        for k, net in enumerate(reversed(self.nets)):
+            parity = (len(self.nets) - 1 - k) % 2
             x[parity], log0 = self.atomic_reverse(
-                                                  x_active=x[parity],
-                                                  x_frozen=x[1 - parity],
-                                                  parity=parity,
-                                                  net=self.nets[k],
-                                                  log0=log0
-                                                  )
+                x_active=x[parity],
+                x_frozen=x[1 - parity],
+                parity=parity,
+                net=net,
+                log0=log0
+            )
         return self.mask.cat(*x), log0
 
     @abstractmethod
@@ -93,14 +93,6 @@ class Coupling_(Module_, ABC):
 
     def postprocess(self, x):
         return x.squeeze(self.channels_axis)
-
-    def transfer(self, scale_factor=1, mask=None, **extra):
-        return self.__class__(
-                [net.transfer(scale_factor=scale_factor) for net in self.nets],
-                mask=self.mask if mask is None else mask,
-                label=self.label,
-                channels_axis=self.channels_axis
-                )
 
 
 # =============================================================================
@@ -156,127 +148,72 @@ class RQSplineCoupling_(Coupling_):
     >>> extrap = {'left': 'anti', 'right': 'linear'}
     """
 
-    def __init__(self, nets, *, mask,
-            xlim=(0, 1), ylim=(0, 1), knots_x=None, knots_y=None, extrap={},
-            **kwargs
-            ):
+    def __init__(
+        self, nets, *, mask,
+        xlim=(0, 1), ylim=(0, 1), knots_x=None, knots_y=None, extrap=None,
+        channels_axis=1
+    ):
 
-        super().__init__(nets, mask=mask, **kwargs)
+        super().__init__(nets, mask=mask, channels_axis=channels_axis)
 
-        self.xlim, self.xwidth = xlim, xlim[1] - xlim[0]
-        self.ylim, self.ywidth = ylim, ylim[1] - ylim[0]
-        self.knots_x = knots_x
-        self.knots_y = knots_y
-        self.extrap = extrap
+        self.spline_kwargs = {
+            'xlim': xlim,
+            'ylim': ylim,
+            'knots_axis': channels_axis,
+            'knots_x': knots_x,
+            'knots_y': knots_y,
+            'extrap': extrap
+        }
 
-        self.softmax = torch.nn.Softmax(dim=self.channels_axis)
-        self.softplus = torch.nn.Softplus(beta=np.log(2))
-        # we set the beta of Softplus to log(2) so that self.softplust(0)
-        # returns 1. With this setting it would be easy to set the derivatives
-        # to 1 (with zero inputs).
+    def make_rq_spline_field(self, feature_map):
+        """Calls `make_rq_spline_field` with the saved key-word argumentes."""
+        return make_rq_spline_field(feature_map, **self.spline_kwargs)
 
     def atomic_forward(self, *, x_active, x_frozen, parity, net, log0=0):
-        out = net(self.preprocess_fz(x_frozen))
-        spline = self.make_spline(out)
+        feature_map = net(self.preprocess_fz(x_frozen))
+        spline = self.make_rq_spline_field(feature_map)
+
         # below g is the gradient of spline @ x_active
         fx_active, g = spline(self.preprocess(x_active), grad=True)
         fx_active, g = self.postprocess(fx_active), self.postprocess(g)
         # the above two lines are equivalent to the following for default cases
         # fx_active, g = spline(x_active, grad=True, squeezed=True)
+
         fx_active = self.mask.purify(fx_active, channel=parity)
         logg = self.mask.purify(torch.log(g), channel=parity)
+
         return fx_active, log0 + self.sum_density(logg)
 
     def atomic_reverse(self, *, x_active, x_frozen, parity, net, log0=0):
-        out = net(self.preprocess_fz(x_frozen))
-        spline = self.make_spline(out)
+        feature_map = net(self.preprocess_fz(x_frozen))
+        spline = self.make_rq_spline_field(feature_map)
+
         # below g is the gradient of spline @ x_active
         fx_active, g = spline.reverse(self.preprocess(x_active), grad=True)
         fx_active, g = self.postprocess(fx_active), self.postprocess(g)
         # the above two lines are equivalent to the following for default cases
         # fx_active, g = spline.reverse(x_active, grad=True, squeezed=True)
+
         fx_active = self.mask.purify(fx_active, channel=parity)
         logg = self.mask.purify(torch.log(g), channel=parity)
+
         return fx_active, log0 + self.sum_density(logg)
 
     def _hack(self, *, x_active, x_frozen, parity, net):
-        out = net(self.preprocess_fz(x_frozen))
-        spline = self.make_spline(out)
+        feature_map = net(self.preprocess_fz(x_frozen))
+        spline = self.make_rq_spline_field(feature_map)
         fx_active, g = spline(self.preprocess(x_active), grad=True)
         fx_active, g = self.postprocess(fx_active), self.postprocess(g)
         fx_active = self.mask.purify(fx_active, channel=parity)
         logg = self.mask.purify(torch.log(g), channel=parity)
         return spline, fx_active, logg
 
-    def make_spline(self, out):
-        # `out` is the output of net(in)
-        """construct a spline with number of knots deduced from input `out`.
-        The first knot is always at `(xlim[0], ylim[0])` and the last knot is
-        always at `(xlim[1], ylim[1])`; hence, the number of channels in the
-        input `out` should always be `3 m - 2` unless one fixes knots_x or
-        knots_y. Here, `m` is the number of knots in the spline.
-
-        To clarify more, the input `out` gets split into (m-1, m-1, m) parts
-        corresponding to knots_x, knots_y, and knots_d.
-        When either knots_x or knots_y is already fixed, the input `out` gets
-        split into (m-1, m) parts and if both are fixed ther will be no
-        partitioning.
-        """
-
-        axis = self.channels_axis
-        knots_x = self.knots_x
-        knots_y = self.knots_y
-
-        def zeropad(w):
-            pad_shape = list(w.shape)
-            pad_shape[axis] = 1  # note that axis migh be e.g. -1
-            return torch.zeros(pad_shape, device=w.device)
-
-        cumsumsoftmax = lambda w: torch.cumsum(self.softmax(w), dim=axis)
-        to_coord = lambda w: torch.cat((zeropad(w), cumsumsoftmax(w)), dim=axis)
-        to_deriv = lambda d: self.softplus(d) if d is not None else None
-
-        n = out.shape[axis]  # n parameters to specify splines
-        if knots_x is None and knots_y is None:
-            m = (n + 2) // 3
-            x_, y_, d_ = out.split((m-1, m-1, m), dim=axis)
-            knots_x = to_coord(x_) * self.xwidth + self.xlim[0]
-            knots_y = to_coord(y_) * self.ywidth + self.ylim[0]
-            knots_d = to_deriv(d_)
-        elif knots_x is not None and knots_y is None:
-            m = (n + 2) // 2
-            y_, d_ = out.split((m-1, m), dim=axis)
-            knots_y = to_coord(y_) * self.ywidth + self.ylim[0]
-            knots_d = to_deriv(d_)
-        elif knots_x is None and knots_y is not None:
-            m = (n + 2) // 2
-            x_, d_ = out.split((m-1, m), dim=axis)
-            knots_x = to_coord(x_) * self.xwidth + self.xlim[0]
-            knots_d = to_deriv(d_)
-        else:
-            knots_d = to_deriv(out)
-
-        kwargs = dict(knots_x=knots_x, knots_y=knots_y, knots_d=knots_d)
-        kwargs.update(dict(knots_axis=axis, extrap=self.extrap))
-
-        return RQSpline(**kwargs)
-
-    def transfer(self, scale_factor=1, mask=None, **extra):
-        return self.__class__(
-                [net.transfer(scale_factor=scale_factor) for net in self.nets],
-                mask=self.mask if mask is None else mask,
-                label=self.label,
-                channels_axis=self.channels_axis,
-                xlim=self.xlim,
-                ylim=self.ylim,
-                knots_x=self.knots_x,
-                knots_y=self.knots_y,
-                extrap=self.extrap
-                )
-
 
 # =============================================================================
 class MultiRQSplineCoupling_(Coupling_):
+
+    # TODO: should be rewritten using make_rq_spline_field()
+
     """A Coupling_ with multi rational quadratic spline transformations,
     each actiong on an additional channel of the input data.
 
@@ -289,11 +226,12 @@ class MultiRQSplineCoupling_(Coupling_):
     For more details on using these options see RQSplineCoupling_ and RQSpline.
     """
 
-    def __init__(self, nets, *, mask,
-            xlims=[(0, 1), (0, 1)], ylims=[(0, 1), (0, 1)],
-            knots_x=[None, None], knots_y=[None, None], extraps=[{}, {}],
-            **kwargs
-            ):
+    def __init__(
+        self, nets, *, mask,
+        xlims=[(0, 1), (0, 1)], ylims=[(0, 1), (0, 1)],
+        knots_x=[None, None], knots_y=[None, None], extraps=[{}, {}],
+        **kwargs
+    ):
 
         super().__init__(nets, mask=mask, **kwargs)
 
@@ -356,6 +294,7 @@ class MultiRQSplineCoupling_(Coupling_):
                 )
 
         axis = self.channels_axis
+
         def zeropad(w):
             pad_shape = list(w.shape)
             pad_shape[axis] = 1  # note that axis migh be e.g. -1
@@ -421,16 +360,3 @@ class MultiRQSplineCoupling_(Coupling_):
             x_actives_out.append(x_active)
             gs.append(g)
         return x_actives_out, gs
-
-    def transfer(self, scale_factor=1, mask=None, **extra):
-        return self.__class__(
-                [net.transfer(scale_factor=scale_factor) for net in self.nets],
-                mask=self.mask if mask is None else mask,
-                label=self.label,
-                channels_axis=self.channels_axis,
-                xlim=self.xlims,
-                ylim=self.ylims,
-                knots_x=self.knots_x,
-                knots_y=self.knots_y,
-                extrap=self.extraps
-                )
