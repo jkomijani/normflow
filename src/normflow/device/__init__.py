82bcd6ffad1d410f7689c3e3d3c9f2f330375d97
Javad Komijani <jkomijani@gmail.com>
2025-11-24 10:13:02 +0100

    feat: do NOT change default torch dtype & device globally; it should be done by users in their scripts if needed.

--- src/normflow/device/__init__.py
+++ src/normflow/device/__init__.py
@@ -4,10 +4,10 @@ import torch
 from ._core import ModelDeviceHandler
 
 
-if torch.cuda.is_available():
-    torch_device = 'cuda'
-else:
-    torch_device = 'cpu'
+# if torch.cuda.is_available():
+#     torch_device = 'cuda'
+# else:
+#     torch_device = 'cpu'
 
-torch.set_default_device(torch_device)
-torch.set_default_dtype(torch.float64)  # double precision
+# torch.set_default_device(torch_device)
+# torch.set_default_dtype(torch.float64)  # double precision
