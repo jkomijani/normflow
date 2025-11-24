ab39bf5be0552d6f5a42d5f2ff7f739422a89090
Javad Komijani <jkomijani@gmail.com>
2025-11-24 08:38:36 +0100

    feat: add NormalSUnPrior, change SUnPrior -> UniformSUnPrior, aliased to SUnPrior

--- src/normflow/prior/__init__.py
+++ src/normflow/prior/__init__.py
@@ -1,4 +1,4 @@
 from .prior import PriorList
 from .prior import NormalPrior, UniformPrior
-from .unitary_prior import UnPrior, SUnPrior, U1Prior
+from .unitary_prior import *
 from .ginibre_prior import GinibrePrior
--- src/normflow/prior/unitary_prior.py
+++ src/normflow/prior/unitary_prior.py
@@ -1,22 +1,33 @@
-# Copyright (c) 2021-2024 Javad Komijani
+# Copyright (c) 2021-2025 Javad Komijani
 
 """This module is for introducing unitary priors."""
 
+from typing import Tuple
+import torch
 
-from .prior import Prior
+from .prior import Prior, NormalPrior
 from ..lib.stats import UnGroup, SUnGroup, U1Group
+from ..lib.matrix_handles import SU2Algebra2Group_
+from ..lib.matrix_handles import SU3Algebra2Group_
 
 
-class UnPrior(Prior):
-    """Generate random unitary matrices, i.e. random U(n)."""
+__all__ = [
+    "UniformUnPrior", "UniformSUnPrior", "UniformU1Prior",
+    "UnPrior", "SUnPrior", "U1Prior",  # alias for legacy
+    "NormalSUnPrior"
+]
 
-    def __init__(self,
-            n: int,
-            shape = (1,),
-            drop_constant_log_prob = False,
-            **super_kwargs
-            ):
 
+class UniformUnPrior(Prior):
+    """Generate unitary matrices uniformly with the Haar measure."""
+
+    def __init__(
+        self,
+        n: int,
+        shape: Tuple = (1,),
+        drop_constant_log_prob: bool = False,
+        **super_kwargs
+    ):
         kws = dict(shape=shape, drop_constant_log_prob=drop_constant_log_prob)
         dist = UnGroup(n, **kws)
 
@@ -40,16 +51,16 @@ class UnPrior(Prior):
         return dict(loc=dist.loc, scale=dist.scale)
 
 
-class SUnPrior(Prior):
-    """Generate random special unitary matrices, i.e. random SU(n)."""
+class UniformSUnPrior(Prior):
+    """Generate SU(n) matrices uniformly with the Haar measure."""
 
-    def __init__(self,
-            n: int,
-            shape = (1,),
-            drop_constant_log_prob = False,
-            **super_kwargs
-            ):
-
+    def __init__(
+        self,
+        n: int,
+        shape: Tuple = (1,),
+        drop_constant_log_prob: bool = False,
+        **super_kwargs
+    ):
         kws = dict(shape=shape, drop_constant_log_prob=drop_constant_log_prob)
         dist = SUnGroup(n, **kws)
 
@@ -73,12 +84,11 @@ class SUnPrior(Prior):
         return dict(loc=dist.loc, scale=dist.scale)
 
 
-class U1Prior(Prior):
-    """Generate random unitary matrices, i.e. random U(1).
+class UniformU1Prior(Prior):
+    """Generate U(1) variables uniformly with the Haar measure.
 
     This is a faster implementation of random U(1) than `UnPrior(n=1)`.
     """
-
     def __init__(self, shape=(1,), **kwargs):
         dist = U1Group(shape=shape)
         super().__init__(dist, **kwargs)
@@ -98,3 +108,63 @@ class U1Prior(Prior):
         """Returns all parameters needed to define the prior in a dict."""
         dist = self.dist.uniform_dist
         return dict(loc=dist.loc, scale=dist.scale)
+
+
+class NormalSUnPrior(NormalPrior):
+    """Generate SU(n) matrices by exponentiating normal-distributed algebra
+    elements.
+
+    This class is a subclass of `NormalPrior` where the innermost dimension of
+    the shape is fixed to `n^2 - 1``, the dimension of the Lie algebra `su(n)`.
+    Samples drawn from the underlying normal distribution are interpreted as
+    algebra elements and then mapped to SU(n) via `alg_to_grp`. The associated
+    log-Jacobian correction is applied both when sampling and when evaluating
+    log-probabilities.
+
+    Args:
+        n: Dimension of the SU(n) group (supports n=2 or n=3).
+        shape: Batch shape for sampling algebra elements.
+        super_kwargs: Passed to ``NormalPrior``. May include:
+            * ``loc``: Mean of the underlying normal distribution.
+            * ``scale``: Stddev of the underlying normal distribution.
+            * ``seed``: Random seed for reproducible sampling.
+    """
+    def __init__(self, n: int, shape: Tuple = (1,), **super_kwargs):
+
+        super().__init__(shape=(*shape, n**2 - 1), **super_kwargs)
+
+        alg_to_grp_kwargs = {
+            'coordinate_representation': True, 'makesure_invertible': False
+        }
+        if n == 2:
+            self.alg_to_grp = SU2Algebra2Group_(**alg_to_grp_kwargs)
+        elif n == 3:
+            self.alg_to_grp = SU3Algebra2Group_(**alg_to_grp_kwargs)
+        else:
+            raise ValueError("Only n = 2, 3 are supported.")
+
+    def sample(self, batch_size: int = 1):
+        """Return samples of SU(n) matrices.
+
+        Args:
+            batch_size: Number of samples.
+        """
+        return self.sample_(batch_size)[0]
+
+    def sample_(self, batch_size: int = 1):
+        """Return samples of SU(n) matrices and log probabilites.
+
+        Args:
+            batch_size: Number of samples.
+        """
+        x, logr = super().sample_(batch_size)  # from normal distribution
+        x, logj = self.alg_to_grp(x)
+        return x, logr - logj
+
+    # def log_prob(self, x):  do NOT do it: the super one needed in NormalPrior
+
+
+# aliased for legace
+UnPrior = UniformUnPrior
+SUnPrior = UniformSUnPrior
+U1Prior = UniformU1Prior
