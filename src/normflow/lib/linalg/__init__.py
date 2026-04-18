645c59307655e12dfcffceb013dbcc79cd420df0
Javad Komijani <jkomijani@gmail.com>
2026-04-18 13:43:53 +0200

    remove special_svd; import lattice_ml.linalg.svd as compute_svd

--- src/normflow/lib/linalg/__init__.py
+++ src/normflow/lib/linalg/__init__.py
@@ -5,7 +5,7 @@
 
 import torch
 
-from lattice_ml.linalg import svd
+from lattice_ml.linalg import svd as compute_svd
 
 
 from .eig_decomposition_ import eigh
@@ -22,32 +22,3 @@ from .qr_decomposition import haar_qr, haar_sqr
 
 from .euler_angles import su2_to_euler_angles
 from .euler_angles import euler_angles_to_su2
-
-
-class AttributeDict4SVD:
-    """For accessing a dict key like an attribute."""
-
-    def __init__(self, **dict_):
-        self.__dict__.update(**dict_)
-
-    def __repr__(self):
-        str_ = "svd:\n"
-        for key, value in self.__dict__.items():
-            str_ += f"{key}={value}\n"
-        return str_
-
-
-def special_svd(matrix):
-    """Return a new svd object, in which U is scaled by a phase, and called sU,
-    where s stands for special, such that sU @ Vh is special unitary.
-    """
-    svd_ = svd(matrix)
-    rdet_angle = torch.angle(torch.det(matrix)) / svd_.U.shape[-1]  # r: rooted
-    phase_factor = torch.exp(-1j * rdet_angle.reshape(*rdet_angle.shape, 1, 1))
-    s_uvh = (svd_.U @ svd_.Vh) * phase_factor  # s stands for special
-    # s_u = svd_.U * torch.exp(-1j * phase_factor)
-    sigma_matrix = svd_.Vh.adjoint() @ (svd_.S.unsqueeze(-1) * svd_.Vh)
-    return AttributeDict4SVD(
-        U=svd_.U, S=svd_.S, Vh=svd_.Vh, rdet_angle=rdet_angle, sUVh=s_uvh,
-        Sigma=sigma_matrix
-        )
